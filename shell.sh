fe71c3fb73b954fb4e145d133dfa20376c88c551
Daniel Nghiem <nghiem.daniel@yahoo.com>
2019-12-12 06:28:12 -0800

    more apps

---
+++ shell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install my preferred shell and settings
+
+# install oh my zsh
+sh -c "$(wget
+https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+-O -)"i
